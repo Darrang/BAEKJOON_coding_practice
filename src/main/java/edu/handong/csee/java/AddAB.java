3e9104b5e856321d030eec60ab83960604419e79
Kimdayoung51 <dayong414@gmail.com>
2020-07-22 21:57:17 +0900

    revise AddAB class

--- src/main/java/edu/handong/csee/java/AddAB.java
+++ src/main/java/edu/handong/csee/java/AddAB.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 public class AddAB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner stdIn = new Scanner(System.in);
-		int A = stdIn.nextInt();
-		int B = stdIn.nextInt();
-	   System.out.println( A+B );
-
+		System.out.print("A의 값:");  int A = stdIn.nextInt();
+		System.out.print("B의 값:");  int B = stdIn.nextInt();
+		int add = A+B;
+		System.out.println( "A와 B의 합: " + add);
 	}
 
 }
