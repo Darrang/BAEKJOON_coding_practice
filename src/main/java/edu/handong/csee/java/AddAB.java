64439eb29034f083d601c25c5f6508270bef3645
Kimdayoung51 <dayong414@gmail.com>
2020-07-22 21:16:36 +0900

    first commit

---
+++ src/main/java/edu/handong/csee/java/AddAB.java
@@ -0,0 +1,15 @@
+package edu.handong.csee.java;
+import java.util.Scanner;
+
+public class AddAB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		int A = stdIn.nextInt();
+		int B = stdIn.nextInt();
+	   System.out.println( A+B );
+
+	}
+
+}
